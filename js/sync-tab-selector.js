2f6ce1c438aeb29b6f7fcf95af42d8f3129dba5b
JobId 650123590 <gitlab-ci@scaledynamics.io>
2020-07-22 08:03:17 +0000

    Publish 221 from ScaleDynamics/prod/warpjs.dev
    (from commit 29efd3f4a4836d4b708643932b07c0f59f1790c3 @ master)

---
+++ js/sync-tab-selector.js
@@ -0,0 +1,52 @@
+window.addEventListener("load", function () {
+  const groups = {};
+
+  // init choice from URL
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const selectedTab = urlSearchParams.get("choice");
+
+  // synchronize all tabs in the page when selecting a tab
+  document.querySelectorAll(".tabs .nav-tabs").forEach(function (tabs) {
+    const navLinks = tabs.querySelectorAll(".nav-link");
+    navLinks.forEach(function (navLink) {
+      // apply selected tab
+      if (selectedTab === navLink.textContent) {
+        setTimeout(function () {
+          navLink.click();
+        });
+      }
+
+      // set group of nav-links
+      const group = navLink.dataset.group;
+      if (!groups[group]) {
+        groups[group] = [];
+      }
+      groups[group].push(navLink);
+
+      function synchronizeTabs() {
+        setTimeout(function () {
+          const activeTab = tabs.querySelector(".nav-link.active");
+
+          // update url
+          urlSearchParams.set("choice", activeTab.textContent);
+          const url = window.location.pathname + "?" + urlSearchParams.toString();
+          history.pushState(null, "", url);
+
+          // sync tabs
+          const currentGroup = activeTab.dataset.group;
+          Object.keys(groups).forEach(function (group) {
+            if (currentGroup === group) {
+              return;
+            }
+            groups[group].forEach(function (navLink) {
+              if (navLink.textContent === activeTab.textContent && !Object.values(navLink.classList).includes("active")) {
+                navLink.click();
+              }
+            });
+          });
+        });
+      }
+      navLink.onclick = synchronizeTabs;
+    });
+  });
+});
